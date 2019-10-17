32421ccf92f30950b38f0c78b7a84902941df742
eriatlovj <56607475+eriatlovj@users.noreply.github.com>
2019-10-16 20:02:32 -0700

    Basic_HTML_and_JavaScript_1
    Basic_HTML_and_JavaScript_1 upload

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang=“en”>
+	<head>
+		<title>Write title here.</title>
+		<meta charset=“utf-8”>
+	</head>
+    <body>
+    function My_First_Function() {
+        var String = “Kiss me, I’m Irish!”;
+        var result = String.fontcolor(“green”);
+        Document.getElementById(“Irish”).innerHTML = result;
+        }
+	<script> src="Hello_World.js"></script>
+	</body>
+</html>
